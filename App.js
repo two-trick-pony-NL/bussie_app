f51297fac52e7466760b2d9c1fe71ae8488f4d0d
Peter van Doorn <pvandoorn@bunq.com>
2022-10-20 19:29:55 +0200

    added location of the user and a pin

--- App.js
+++ App.js
@@ -1,12 +1,53 @@
-import * as React from 'react';
-import MapView from 'react-native-maps';
+import React, { useState, useEffect } from 'react';
+import MapView, { Callout } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import * as Location from 'expo-location';
+import { Marker } from 'react-native-maps';
 
 export default function App() {
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let user_location = 'Waiting..';
+  let user_longitude = 0;
+  let user_latitude = 0;
+
+  if (errorMsg) {
+    user_location = errorMsg;
+  } else if (location) {
+    user_location = JSON.stringify(location)
+    user_longitude = parseFloat(JSON.stringify(location.coords.longitude));
+    user_latitude = parseFloat(JSON.stringify(location.coords.latitude));
+  }
+
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} />
+      <MapView style={styles.map}>
+        <Marker coordinate={{latitude: user_latitude, longitude: user_longitude}}>
+        <Callout> 
+          <Text>"This is you!"</Text>
+        </Callout>
+        </Marker>
+
+
+
+      </MapView>
     </View>
+    
   );
 }
 
