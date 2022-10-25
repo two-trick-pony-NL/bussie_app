d1049165bd23cd07d67c6bd72aa6442fb38afdbc
Peter van Doorn <pvandoorn@bunq.com>
2022-10-25 11:04:36 +0200

    Better spacing of Station names

--- components/table.js
+++ components/table.js
@@ -9,70 +9,48 @@ const Table = () =>{
             <View style={styles.container}>
                 <DataTable>
                 <DataTable.Header>
-                    <DataTable.Title>Lijn:</DataTable.Title>
-                    <DataTable.Title >Aankomst in:</DataTable.Title>
-                    <DataTable.Title >Naar:</DataTable.Title>
+                    <DataTable.Title style={{flex: 1}}>Lijn:</DataTable.Title>
+                    <DataTable.Title style={{flex: 1}} >in:</DataTable.Title>
+                    <DataTable.Title style={{flex: 2}} >Naar:</DataTable.Title>
                 </DataTable.Header>
 
                 <DataTable.Row>
-                    <DataTable.Cell >73</DataTable.Cell>
-                    <DataTable.Cell >176 s</DataTable.Cell>
-                    <DataTable.Cell>Zeist</DataTable.Cell>       
+                    <DataTable.Cell style={{flex: 1}}>🚌 73</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>176 s</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 2}}>Zeist</DataTable.Cell>       
                 </DataTable.Row>
 
                 <DataTable.Row>
-                    <DataTable.Cell >73</DataTable.Cell>
-                    <DataTable.Cell >6 s</DataTable.Cell>
-                    <DataTable.Cell>Maarssen</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>🚌 73</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>6 s</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 2}}>Maarssen</DataTable.Cell>
                 </DataTable.Row>
 
                 <DataTable.Row>
-                    <DataTable.Cell >28</DataTable.Cell>
-                    <DataTable.Cell >65 s</DataTable.Cell>
-                    <DataTable.Cell>Vleuterweide (Winkelcentrum)</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>🚈 28</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>65 s</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 2}}>Vleuterweide (Winkelcentrum)</DataTable.Cell>
                 </DataTable.Row>
 
                 <DataTable.Row>
-                    <DataTable.Cell >28</DataTable.Cell>
-                    <DataTable.Cell >405 s</DataTable.Cell>
-                    <DataTable.Cell>De Uithof (Science Park)</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>🚈 28</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>405 s</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 2}}>De Uithof (Science Park)</DataTable.Cell>
                 </DataTable.Row>
 
                 <DataTable.Row>
-                    <DataTable.Cell >5</DataTable.Cell>
-                    <DataTable.Cell >6 s</DataTable.Cell>
-                    <DataTable.Cell>Oog in Al</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>🚅 5</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>6 s</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 2}}>Den Haag (Centraal)</DataTable.Cell>
                 </DataTable.Row>
 
                 <DataTable.Row>
-                    <DataTable.Cell >73</DataTable.Cell>
-                    <DataTable.Cell >176 s</DataTable.Cell>
-                    <DataTable.Cell>Zeist</DataTable.Cell>       
+                    <DataTable.Cell style={{flex: 1}}>🚇 58</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 1}}>54 s</DataTable.Cell>
+                    <DataTable.Cell style={{flex: 2}}>Schiphol Airport</DataTable.Cell>
                 </DataTable.Row>
 
-                <DataTable.Row>
-                    <DataTable.Cell >73</DataTable.Cell>
-                    <DataTable.Cell >6 s</DataTable.Cell>
-                    <DataTable.Cell>Maarssen</DataTable.Cell>
-                </DataTable.Row>
-
-                <DataTable.Row>
-                    <DataTable.Cell >28</DataTable.Cell>
-                    <DataTable.Cell >65 s</DataTable.Cell>
-                    <DataTable.Cell>Vleuterweide (Winkelcentrum)</DataTable.Cell>
-                </DataTable.Row>
-
-                <DataTable.Row>
-                    <DataTable.Cell >28</DataTable.Cell>
-                    <DataTable.Cell >405 s</DataTable.Cell>
-                    <DataTable.Cell>De Uithof (Science Park)</DataTable.Cell>
-                </DataTable.Row>
-
-                <DataTable.Row>
-                    <DataTable.Cell >5</DataTable.Cell>
-                    <DataTable.Cell >6 s</DataTable.Cell>
-                    <DataTable.Cell>Oog in Al</DataTable.Cell>
-                </DataTable.Row>
+                
 
                
                
